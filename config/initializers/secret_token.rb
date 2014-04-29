3562a8596f20f9f6f3a7eafa2e67ae7a636c6a6f
Manuel Omar Pineyro <student@epicodus.com>
2014-04-29 10:32:59 -0700

    Setup new rails application and begin implementing ember

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RestaurantOrdersEmberRails::Application.config.secret_key_base = '9ac7fc195e7f04f6c54f3e419a54bda6c1e8c4b9e0568d1a9a32f988fa3cad11c93edf981562b13fc65df1f131468d6bcfd68c74068085d305ab6b12e7b76419'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RestaurantOrdersEmberRails::Application.config.session_store :cookie_store, key: '_restaurant_orders_ember_rails_session'
